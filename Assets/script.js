8d2c059709735c501071913d9bf341e6a548f9e9
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-09-12 11:17:27 -0500

    Changed the id on line 3 in js to assign to the appropriate p tag to display today. Moved constant currallTimeBlocks out to try and make the other elements append to it. Currently not doing so.

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,14 @@
 const currentHour = moment().hour();
-$('#today').text(moment().format("dddd" + "MMMM Do, YYYY, h:m A"));
+const allTimeBlocks = $('.container');
+$('#currentDay').text(moment().format("dddd " + "MMMM Do, YYYY, h:m A"));
 
 function appendTimeBlocks(){
-    const allTimeBlocks = $('.container');
-    const timeBlock = $('<div></div>').text['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM']; //.addClass('.hour');
+   
+    const timeBlock = $('<div></div>').text('9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM').addClass('.hour');
     const textAreas = $('<textarea></textarea>').attr('Placeholder', 'Enter event here...');
     const saveBtns = $('<button></button>').addClass('.saveBtn');
 
-    $('body').append(allTimeBlocks, timeBlock, textAreas, saveBtns);
+    $('.container').append(allTimeBlocks, timeBlock, textAreas, saveBtns);
 
 
 for (let i = 0; i < 9; i++){
