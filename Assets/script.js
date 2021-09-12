e2cf0d23b2776306801b46d93c2b93663c84a4b3
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-09-12 10:49:46 -0500

    Created several constants to try and create new element tags for each item I need.

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,9 @@
-const timeBlocks = $('.container');
+const allTimeBlocks = $('.container');
+const timeBlock = $('<div></div>').text['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM'].addClass('.hour');
+const currentHour = moment().hour();
+const eventHour = timeBlock[i];
+const textAreas = $('<textarea></textarea>').attr('Placeholder', 'Enter event here...');
+const saveBtns = $('<button></button>').addClass('.saveBtn');
 
 $('#today').text(moment().format("dddd" + "MMMM Do, YYYY, h:m A"));
 
