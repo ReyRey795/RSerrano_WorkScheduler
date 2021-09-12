f77afd3086489fff2a3d5d803b49812988884443
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-09-12 11:08:54 -0500

    I've situated the certain constants to a function to append their information, and replaced information inside the for loop

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,18 @@
-const allTimeBlocks = $('.container');
-const timeBlock = $('<div></div>').text['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM'].addClass('.hour');
 const currentHour = moment().hour();
-const eventHour = timeBlock[i];
-const textAreas = $('<textarea></textarea>').attr('Placeholder', 'Enter event here...');
-const saveBtns = $('<button></button>').addClass('.saveBtn');
-
 $('#today').text(moment().format("dddd" + "MMMM Do, YYYY, h:m A"));
 
-for (let i = 0; i < 9; i++){
+function appendTimeBlocks(){
+    const allTimeBlocks = $('.container');
+    const timeBlock = $('<div></div>').text['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM']; //.addClass('.hour');
+    const textAreas = $('<textarea></textarea>').attr('Placeholder', 'Enter event here...');
+    const saveBtns = $('<button></button>').addClass('.saveBtn');
 
-}
+    $('body').append(allTimeBlocks, timeBlock, textAreas, saveBtns);
+
+
+for (let i = 0; i < 9; i++){
+    const eachTimeBlock = timeBlock[i];
+    console.log(i);
+
+
+}}
