442675c98588f5bb0068256074c730219a2a93ce
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-09-13 18:35:42 -0500

    This is a commit on the work I was doing with Peter. We also added a if else statement to add the classes of the needed time eras.

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,31 @@
 const currentHour = moment().hour();
 const allTimeBlocks = document.querySelector('#container');
-const workHours = ['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM'];
+const arrayOfObjects = [{time:'9:00 AM', mtime:9}, {time:'10:00 AM', mtime:10}, {time:'11:00 AM', mtime:11}, {time:'12:00 PM', mtime:12},
+ {time:'1:00 PM', mtime:13}, {time:'2:00 PM', mtime:14}, {time:'3:00 PM', mtime:15}, {time:'4:00 PM', mtime:16}, {time:'5:00 PM', mtime:17}];
 $('#currentDay').text(moment().format("dddd " + "MMMM Do, YYYY, h:m A"));
 
-for (let i = 0; i < workHours.length; i++){
+for (let i = 0; i < arrayOfObjects.length; i++){
 
-    const timeBlocks = workHours[i]
+    const timeBlocks = arrayOfObjects[i]
     console.log(i);
-
+    const timeBlockHour = timeBlocks.mtime;
     const timeBlock = document.createElement('div');
     const textAreas = document.createElement('textarea');
     const saveBtns = document.createElement('button');
 
-    timeBlock.textContent = timeBlocks;
+    timeBlock.textContent = timeBlocks.time;
     timeBlock.appendChild(textAreas);
     timeBlock.appendChild(saveBtns);
+
+    if (currentHour > timeBlockHour) {
+        $(this).addClass('past')
+    }
+    else if (currentHour < timeBlockHour) {
+        $(this).addClass('future')
+    }
+    else if (currentHour === timeBlockHour){
+        $(this).addClass('current')
+    }
 
     allTimeBlocks.appendChild(timeBlock);
 
@@ -44,8 +55,8 @@ $('button').each(
 
 
 function hTrail(){
-    console.log(timeBlocks,currentHour)
-    if (workHours[i] == currentHour)
+    console.log(arrayOfObjects, currentHour)
+    if (arrayOfObjects[i] == currentHour)
     $(this).addClass('present')
     
 }};
