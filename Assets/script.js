36299307941f7ca519c0e4147dfc5e74e8340deb
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-09-13 20:42:06 -0500

    Continued working through the javascript functions to get the localStorage situated, as well as adding a function to apply the necessary classes to their perspective timeblocks

--- Assets/script.js
+++ Assets/script.js
@@ -1,58 +1,55 @@
 const currentHour = moment().hour();
-const allTimeBlocks = document.querySelector('#container');
+const timeBlocks = $('#container');
 const arrayOfObjects = [{time:'9:00 AM', mtime:9}, {time:'10:00 AM', mtime:10}, {time:'11:00 AM', mtime:11}, {time:'12:00 PM', mtime:12},
  {time:'1:00 PM', mtime:13}, {time:'2:00 PM', mtime:14}, {time:'3:00 PM', mtime:15}, {time:'4:00 PM', mtime:16}, {time:'5:00 PM', mtime:17}];
 $('#currentDay').text(moment().format("dddd " + "MMMM Do, YYYY, h:m A"));
 
 for (let i = 0; i < arrayOfObjects.length; i++){
 
-    const timeBlocks = arrayOfObjects[i]
+    const timeBlock = arrayOfObjects[i]
     console.log(i);
-    const timeBlockHour = timeBlocks.mtime;
-    const timeBlock = document.createElement('div');
-    const textAreas = document.createElement('textarea');
-    const saveBtns = document.createElement('button');
+    const timeBlockHour = timeBlock.mtime;
+    const timeBlockContainer = $('<div>');
+    const textArea = $('<textarea>');
+    const saveBtn = $('<button>');
 
-    timeBlock.textContent = timeBlocks.time;
-    timeBlock.appendChild(textAreas);
-    timeBlock.appendChild(saveBtns);
+    let iCon = $('<i>').addClass('fas fa-save');
+    
+    timeBlockContainer.addClass('hour');
+
+    timeBlockContainer.text(timeBlock.time);
+    saveBtn.addClass('saveBtn');
+    saveBtn.append(iCon);
+    saveBtn.val(timeBlock.mtime);
+    textArea.attr('id', `textarea-${timeBlock.mtime}`)
+    saveBtn.click(function(){
+        const btnValue = $(this).val()
+        const txtValue = $(`#textarea-${btnValue}`).val()
+        console.log(btnValue)
+        console.log(txtValue)
+        localStorage.setItem(btnValue, txtValue)
+    })
+
+    //if (localStorage.getItem(timeBlock.mtime)) {
+        const xVal = localStorage.getItem(timeBlock.mtime)
+        textArea.val(xVal)
+    //}
+    
+    timeBlockContainer.append(textArea);
+
+    timeBlockContainer.append(saveBtn);
 
     if (currentHour > timeBlockHour) {
-        $(this).addClass('past')
+        timeBlockContainer.addClass('past')
     }
     else if (currentHour < timeBlockHour) {
-        $(this).addClass('future')
+        timeBlockContainer.addClass('future')
     }
     else if (currentHour === timeBlockHour){
-        $(this).addClass('current')
+        timeBlockContainer.addClass('current')
     }
 
-    allTimeBlocks.appendChild(timeBlock);
-
-    $('div').each(
-        function(){
-            $(this).addClass('hour')
-        }
-    )
-$('button').each(
-    function(){
-        $(this).addClass('saveBtn')
-    }
-)
-
-    $('button').each(
-        function(){
-            $(this);document.createElement('i')
-            $('i').each(
-                function(){
-                    $(this).addClass('fas fa-save')
-                }
-            )
-        }
-    )
-
-    
-
+    timeBlocks.append(timeBlockContainer);
 
 function hTrail(){
     console.log(arrayOfObjects, currentHour)
